6a7be2effb7cf67cdcc6800c7b139bac78d1f50c
Dedy Simamora <DS37676X@cimbniaga.co.id>
2020-11-09 22:12:54 +0700

    delete unused file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React,{useEffect, useState} from "react"
 import './App.css';
 
 function App() {
+  
+  // useEffect(() => {
+  //   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+  //   if (navigator.getUserMedia) {
+  //       navigator.getUserMedia({video: true}, handleVideo, videoError);
+  //   }
+  // });
+
+  let joinRoom = () => {
+ 
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
