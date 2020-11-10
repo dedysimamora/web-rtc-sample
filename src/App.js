c11e50327d7b88626dcf9ca90d6d3c735dde144a
Dedy Simamora <DS37676X@cimbniaga.co.id>
2020-11-10 17:22:47 +0700

    success render local video and send to peer

--- src/App.js
+++ src/App.js
@@ -7,16 +7,6 @@ import './App.css';
 
 function App() {
   
-  // useEffect(() => {
-  //   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
-  //   if (navigator.getUserMedia) {
-  //       navigator.getUserMedia({video: true}, handleVideo, videoError);
-  //   }
-  // });
-
-  let joinRoom = () => {
- 
-  }
   return (
     <div className={'main-container'}>
         <VideoContainer/>
---
+++ src/config.js
@@ -0,0 +1,12 @@
+const config = {
+  apiKey: "AIzaSyDk3i5l038BGO1o34rarpyBPIrOvAeEKLg",
+  authDomain: "webrtcsample-cf754.firebaseapp.com",
+  databaseURL: "https://webrtcsample-cf754.firebaseio.com",
+  projectId: "webrtcsample-cf754",
+  storageBucket: "webrtcsample-cf754.appspot.com",
+  messagingSenderId: "60464308022",
+  appId: "1:60464308022:web:6d085b34f3f727fda6dd97"
+}
+
+
+export default config
